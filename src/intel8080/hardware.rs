2c954ef506c0a170574775352d2ec808cdcfb57b
Pedro <185304469+alvespedro0225@users.noreply.github.com>
2025-06-20 20:59:40 -0300

    added flag register and tests

--- src/intel8080/hardware.rs
+++ src/intel8080/hardware.rs
@@ -2,7 +2,7 @@ const MEMORY_SIZE: usize = 2usize.pow(16);
 const REGISTER_NUMBER: usize = 7;
 
 pub struct Intel8080 {
-    memory: [u8; MEMORY_SIZE],
+    pub memory: [u8; MEMORY_SIZE],
     registers: [u8; REGISTER_NUMBER],
     pub stack_pointer: u16,
     pub program_counter: u16,
@@ -20,7 +20,7 @@ impl Default for Intel8080 {
             memory: [0; MEMORY_SIZE],
             stack_pointer: 0,
             program_counter: 0,
-            status: 0,
+            status: 2,
             bc: 0,
             de: 0,
             hl: 0,
@@ -67,7 +67,7 @@ impl Intel8080 {
     }
 
     pub fn set_register(&mut self, register: Register, value: u8) {
-              
+
         let (mut index, paired, first_byte) = Register::value(register);
 
         let paired_pointer = match paired {
@@ -76,28 +76,46 @@ impl Intel8080 {
             PairedRegister::DE => &mut self.de,
             PairedRegister::HL => &mut self.hl,
         };
-        
+
         Self::update_paired_register(paired_pointer, value as u16, first_byte);
-        
+
         if let PairedRegister::PSW = paired {
             // register M
             if index == 6 {
                 self.memory[self.hl as usize] = value;
                 return
-            }        
-            
+            }
+
             index = 6;
-        } 
-        
+        }
+
         self.registers[index] = value;
     }
 
+    pub fn set_status(&mut self, flags: StatusFlags, value: bool) {
+        let offset = match flags {
+            StatusFlags::S => 7,
+            StatusFlags::Z => 6,
+            StatusFlags::AC => 4,
+            StatusFlags::P => 2,
+            StatusFlags::C => 0
+        };
+
+        if value {
+            self.status |= 1 << offset;
+            return
+        }
+
+        self.status &= !(1 << offset);
+    }
     fn update_paired_register(paired_register: &mut u16, value: u16, first_byte: bool) {
         let offset = if first_byte { 8 } else { 0 };
         let mask = !(0xF << offset);
         *paired_register &= mask;
         *paired_register |= value << offset;
     }
+
+
 }
 pub enum Register {
     B,
@@ -117,6 +135,14 @@ enum PairedRegister {
     PSW,
 }
 
+pub enum StatusFlags {
+    S,
+    Z,
+    P,
+    C,
+    AC
+}
+
 impl Register {
     fn value(variation: Self) -> (usize, PairedRegister, bool) {
         match variation {
@@ -134,7 +160,7 @@ impl Register {
 
 pub mod tests {
     use super::*;
-    
+
     #[test]
     fn mutation_first_byte() {
         let mut cpu = Intel8080::default();
@@ -148,21 +174,21 @@ pub mod tests {
         cpu.set_register(Register::L, 0x3);
         assert_eq!(cpu.hl, 0x3)
     }
-    
+
     #[test]
     fn mutate_a(){
         let mut cpu = Intel8080::default();
         cpu.set_register(Register::A, 200);
         assert_eq!(200, cpu.get_register(Register::A))
     }
-    
+
     #[test]
     fn mutate_m_default(){
         let mut cpu = Intel8080::default();
         cpu.set_register(Register::M, 200);
         assert_eq!(cpu.get_register(Register::M), 200)
     }
-    
+
     #[test]
     fn mutate_hl_before_m(){
         let mut cpu = Intel8080::default();
@@ -170,12 +196,27 @@ pub mod tests {
         cpu.set_register(Register::M, 200);
         assert_eq!(cpu.get_register(Register::M), 200);
     }
-    
+
     #[test]
     fn mutate_hl_after_m(){
         let mut cpu = Intel8080::default();
         cpu.set_register(Register::M, 199);
         cpu.set_register(Register::L, 100);
         assert_ne!(199, cpu.get_register(Register::M))
+    }
+
+    #[test]
+    fn set_status_true(){
+        let mut cpu = Intel8080::default();
+        cpu.set_status(StatusFlags::S, true);
+        assert_eq!(130, cpu.status)
+    }
+
+    #[test]
+    fn set_status_false(){
+        let mut cpu = Intel8080::default();
+        cpu.status = 255;
+        cpu.set_status(StatusFlags::AC, false);
+        assert_eq!(239, cpu.status)
     }
 }
